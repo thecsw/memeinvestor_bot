0f3483cb46aca7f015a590b9f2c1bdc56d1d8ba6
robalb <alberto.ventafridda@gmail.com>
2018-05-27 02:07:11 +0200

    added graphs, and grid design body
    todo: getting started section (probably m6 m6) with info about the bot and how to use it. all the links in the navbar should point to divs in the homepage, after the leaderboard.

--- interface/js/main.js
+++ interface/js/main.js
@@ -1,4 +1,21 @@
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
+    
+    
+var data = {
+  // A labels array that can contain any sort of values
+  labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+  // Our series array that contains series objects or in this case series data arrays
+  series: [
+    [5, 2, 4, 2, 0]
+  ]
+};
+
+// Create a new line chart object where as first parameter we pass in a selector
+// that is resolving to our chart container element. The Second parameter
+// is the actual data object.
+new Chartist.Line('.ct-chart', data);
   });
+
+  
