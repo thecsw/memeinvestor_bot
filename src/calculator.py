e032b78495ab9afb37c34b5f257a7c26b1368c3b
rickles42 <81503+rickles42@users.noreply.github.com>
2018-06-28 22:41:34 -0700

    Revamp investment formula and add visualizer
    Lots of work went into the formula rewrite, mainly from jimbobur and
    rickles42.

--- src/calculator.py
+++ src/calculator.py
@@ -1,14 +1,13 @@
 import time
 import datetime
 import logging
-from functools import lru_cache
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import praw
-from fastnumbers import fast_float
 
 import config
+import formula
 import message
 from kill_handler import KillHandler
 from models import Investment, Investor
@@ -36,58 +35,6 @@ def edit_wrap(self, body):
     else:
         logging.info(body)
         return False
-
-# @lru_cache(maxsize=10240)
-def calculate(new, old):
-
-    # Multiplier is detemined by a power function of the relative change in upvotes
-    # since the investment was made.
-    # Functional form: y = x^m ;
-    #    y = multiplier,
-    #    x = relative growth: (change in upvotes) / (upvotes at time of investment),
-    #    m = scale factor: allow curtailing high-growth post returns to make the playing field a bit fairer
-
-    new = fast_float(new)
-    old = fast_float(old)
-
-    # Scale factor for multiplier
-    scale_factor = 1 / fast_float(3)
-
-    # Calculate relative change
-    if old != 0:
-        rel_change = (new - old) / abs(old)
-    # If starting upvotes was zero, avoid dividing by zero
-    else:
-        rel_change = new
-
-    mult = pow((rel_change+1), scale_factor)
-
-    # Investment must grow by more than a threshold amount to win. Decide if
-    # investment was successful and whether you get anything back at all.
-    win_threshold = 1.2
-    if mult > win_threshold:
-        investment_success = True
-        return_money = True
-    elif mult > 1:
-        investment_success = False
-        return_money = True
-    else:
-        investment_success = False
-        return_money = False
-
-    # Investor gains money only if investment was successful. If mult
-    # was below win_threshold but above 1 return factor is ratio of
-    # difference between mult and 1 and difference between win_threshold and 1.
-    # Otherwise, if mult was 1 or lower, get back nothing.
-    if investment_success:
-        factor = mult
-    elif return_money:
-        factor = (mult - 1)/(win_threshold - 1)
-    else:
-        factor = 0
-
-    return factor
-
 
 def main():
     logging.info("Starting calculator")
@@ -149,7 +96,7 @@ def main():
             upvotes_now = post.ups # <--- triggers a Reddit API call
 
             # Updating the investor's balance
-            factor = calculate(upvotes_now, investment.upvotes)
+            factor = formula.calculate(upvotes_now, investment.upvotes)
             amount = investment.amount
             balance = investor.balance
 
---
+++ src/formula.py
@@ -0,0 +1,56 @@
+import math
+
+from fastnumbers import fast_float
+
+def calculate(new, old):
+    new = fast_float(new)
+    old = fast_float(old)
+
+    # Treat anything below 0 upvotes as 0 upvotes
+    if old < 0:
+        old = 0
+    if new < 0:
+        new = 0 
+
+    # Compute gain
+    delta = new - old
+
+    # Treat negative gain as no gain
+    if delta < 0:
+        delta = 0
+
+    # Compute the maximum of the sigmoid
+    sig_max = sigmoid_max(old)
+
+    # Compute the midpoint of the sigmoid
+    sig_mp = sigmoid_midpoint(old)
+
+    # Compute the steepness of the sigmoid
+    sig_stp = sigmoid_steepness(old)
+
+    # Calculate return
+    factor = sigmoid(delta, sig_max, sig_mp, sig_stp)
+
+    return factor
+
+def sigmoid(x, maxvalue, midpoint, steepness):
+    arg = -(steepness * (x - midpoint))
+    y = fast_float(maxvalue) / ( 1 + math.exp(arg) )
+    return y
+
+def sigmoid_max(old):
+    return 5 - 0.8 * math.log10(old+1) if old <= 99999 else 1
+
+def sigmoid_midpoint(old):
+    sig_mp_0 = 75
+    sig_mp_1 = 2000
+    return linear_interpolate(old, 0, 25000, sig_mp_0, sig_mp_1)
+
+def sigmoid_steepness(old):
+    return 0.04 / ((old / 200) + 1)
+
+def linear_interpolate(x, x_0, x_1, y_0, y_1):
+    m = (y_1 - y_0) / fast_float(x_1 - x_0)
+    c = y_0
+    y = (m * x) + c
+    return y
