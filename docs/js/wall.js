6b0b98bbfe6631ede61d6f46a270b3e74a5611cf
robalb <alberto.ventafridda@gmail.com>
2018-07-06 22:49:53 +0200

    adding wall module

--- docs/js/wall.js
+++ docs/js/wall.js
@@ -1,6 +1,32 @@
 import {connectionErrorToast} from './modules/uiElements.js';
 import * as jsonApi from './modules/jsonApi.js';
-import {formatToUnits, iterateDays} from './modules/dataUtils.js';
+import {formatToUnits} from './modules/dataUtils.js';
+
+let leaderboard = (function(){
+   let ids = {
+      buttons: {
+         beta: 'beta-bt',
+         current: 'current-bt'
+      }
+   }
+   function init(){
+      //add buttons click listener
+      let button = document.getElementById(ids.buttons.beta);
+      button.addEventListener('click', e=> loadBeta() );
+      button = document.getElementById(ids.buttons.current);
+      button.addEventListener('click', e=> loadCurrent() );
+      // check if url contains ?s=
+      let url = new URL(window.location.href);
+      let season = url.searchParams.get("s");
+      if (season === 'beta'{
+         loadBeta();
+      }
+      else{
+         loadCurrent();
+      }
+
+   }
+})();
 
 (function(){
    document.addEventListener('DOMContentLoaded', function(){
