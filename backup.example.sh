f903bb48472b9f340355fd89c7e8ab2ad7d0c3f4
Sandy <sagindyk.urazayev@protonmail.ch>
2018-07-20 01:05:44 +0400

    Added a bacup folder and automated the script.

--- backup.example.sh
+++ backup.example.sh
@@ -2,7 +2,7 @@
 _now=$(date +"%m_%d_%Y")
 _file="/tmp/backup_$_now.sql"
 echo "Starting backup to $_file..."
-docker-compose exec mysql mysqldump DATABASE_NAME -u USERNAME -p -r "$_file"
+docker-compose exec mysql mysqldump DATABASE_NAME -u USERNAME -p'password' -r "$_file"
 echo "Done dumping the database. Copying the file to the host machine..."
-docker cp memeinvestor_bot_mysql_1:/tmp/"$_file" .
+docker cp memeinvestor_bot_mysql_1:/tmp/"$_file" ./backups
 echo "Done"
