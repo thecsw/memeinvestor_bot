8a8cff3624f0cfaa93260fafe71e4223551ae153
Sandy <sagindyk.urazayev@protonmail.ch>
2018-07-21 23:08:24 +0400

    Improve backups (#189)
    Backup script puts backups in ./backups

--- backup.sh
+++ backup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+echo "Trying to create a backups folder..."
+mkdir backups
 _now=$(date +"%m_%d_%Y")
 _file="/tmp/backup_$_now.sql"
 echo "Starting backup to $_file..."
-docker-compose exec mysql mysqldump DATABASE_NAME -u USERNAME -p -r "$_file"
+docker-compose exec mysql mysqldump mysql -u mysql -p -r "$_file"
 echo "Done dumping the database. Copying the file to the host machine..."
-docker cp memeinvestor_bot_mysql_1:/tmp/"$_file" .
+docker cp memeinvestor_bot_mysql_1:$_file ./backups
 echo "Done"
