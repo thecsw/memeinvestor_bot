e64de8c37ecef70d0fe06d5daa40b49d2013c814
Sandy <thecsw@ku.edu>
2018-11-22 15:17:46 -0600

    Reduced the number of statements and branches in main
    PEP8 Compliance

--- src/submitter.py
+++ src/submitter.py
@@ -19,11 +19,42 @@ from stopwatch import Stopwatch
 praw.models.Submission.reply_wrap = reply_wrap
 logging.basicConfig(level=logging.INFO)
 
+
+def process_investor(sess, submission):
+    
+    # If a poster doesn't have an account, delete the post
+    # if he has, take 1000 MemeCoins and invest them
+    investor = sess.query(Investor).\
+        filter(Investor.name == submission.author.name).\
+        first()
+    
+    if not investor:
+        bot_reply = submission.reply_wrap(message.NO_ACCOUNT_POST_ORG)
+        delete_post = True
+        logging.info(" -- Not a registered investor!")
+    elif investor.balance < 250:
+        bot_reply = submission.reply_wrap(message.modify_pay_to_post(investor.balance))
+        delete_post = True
+        logging.info(" -- Not enough funds!")
+    else:
+        # We will make it 6%
+        required_fee = int(investor.balance * 0.06)
+        if required_fee < 250:
+            required_fee = 250
+        new_balance = investor.balance - required_fee
+        investor.balance = new_balance
+        bot_reply = submission.\
+        reply_wrap(message.modify_invest_place_here(required_fee))
+        sess.commit()
+
+    return (bot_reply, delete_post,)
+
 def main():
     """
     This is the main function that listens to new submissions
     and then posts the ATTENTION sticky comment.
     """
+    
     killhandler = KillHandler()
 
     engine = create_engine(config.db, pool_recycle=60)
@@ -77,31 +108,7 @@ def main():
             # Post a comment to let people know where to invest
             bot_reply = submission.reply_wrap(message.INVEST_PLACE_HERE_NO_FEE)
         else:
-
-            # If a poster doesn't have an account, delete the post
-            # if he has, take 1000 MemeCoins and invest them
-            investor = sess.query(Investor).\
-                filter(Investor.name == submission.author.name).\
-                first()
-
-            if not investor:
-                bot_reply = submission.reply_wrap(message.NO_ACCOUNT_POST_ORG)
-                delete_post = True
-                logging.info(" -- Not a registered investor!")
-            elif investor.balance < 250:
-                bot_reply = submission.reply_wrap(message.modify_pay_to_post(investor.balance))
-                delete_post = True
-                logging.info(" -- Not enough funds!")
-            else:
-            # We will make it 6%
-                required_fee = int(investor.balance * 0.06)
-                if required_fee < 250:
-                    required_fee = 250
-                new_balance = investor.balance - required_fee
-                investor.balance = new_balance
-                bot_reply = submission.\
-                reply_wrap(message.modify_invest_place_here(required_fee))
-                sess.commit()
+            (bot_reply, delete_post) = process_investor(sess, submission)
 
         # Sticky the comment
         if config.is_moderator:
