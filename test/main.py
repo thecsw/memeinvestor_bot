34d4d885c8df641c89c169b3181ba931180abcad
Matt Bell <mappum@gmail.com>
2018-12-26 20:24:18 -0800

    Added a little more test coverage

--- test/main.py
+++ test/main.py
@@ -8,56 +8,104 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 from comment_worker import CommentWorker
-from models import Base
-from mock_praw import Comment
+from models import Base, Investor
+from mock_praw import Comment, Submission
+import message
 
 class Test(unittest.TestCase):
     def setUp(self):
         # create sqlite db
         engine = create_engine('sqlite:///.testenv/test.db')
-        session_maker = sessionmaker(bind=engine)
+        self.Session = session_maker = sessionmaker(bind=engine)
         Base.metadata.create_all(engine)
 
         self.worker = CommentWorker(session_maker)
 
     def tearDown(self):
-        # reset db
+        # remove db file
         os.remove('.testenv/test.db')
+
+    def command(self, command, username='testuser', post='testpost'):
+        comment = Comment(username, command, submission=Submission(post))
+        self.worker(comment)
+        return comment.replies
 
 class TestUserInit(Test):
     def test_create(self):
-        comment = Comment('testuser', '!create')
-        self.worker(comment)
-        self.assertEqual(len(comment.replies), 1)
+        replies = self.command('!create')
+        self.assertEqual(len(replies), 1)
         self.assertEqual(
-            comment.replies[0].strip(),
+            replies[0].strip(),
             '*Account created!*\n\nThank you testuser for creating a bank account in r/MemeEconomy!\n\nYour starting balance is **1,000 MemeCoins**.'
         )
 
     def test_already_created(self):
-        comment = Comment('testuser', '!create')
-        self.worker(comment)
-
-        comment = Comment('testuser', '!create')
-        self.worker(comment)
-        self.assertEqual(len(comment.replies), 1)
+        replies = self.command('!create')
+        replies = self.command('!create')
+        self.assertEqual(len(replies), 1)
         self.assertEqual(
-            comment.replies[0].strip(),
+            replies[0].strip(),
             'I love the enthusiasm, but you\'ve already got an account!'
         )
 
     def test_autocreate(self):
-        comment = Comment('testuser2', '!balance')
-        self.worker(comment)
-        self.assertEqual(len(comment.replies), 2)
+        replies = self.command('!balance')
+        self.assertEqual(len(replies), 2)
         self.assertEqual(
-            comment.replies[0].strip(),
-            '*Account created!*\n\nThank you testuser2 for creating a bank account in r/MemeEconomy!\n\nYour starting balance is **1,000 MemeCoins**.'
+            replies[0].strip(),
+            '*Account created!*\n\nThank you testuser for creating a bank account in r/MemeEconomy!\n\nYour starting balance is **1,000 MemeCoins**.'
         )
         self.assertEqual(
-            comment.replies[1].strip(),
+            replies[1].strip(),
             'Currently, your account balance is **1,000 MemeCoins**.'
         )
+
+class TestBasic(Test):
+    def test_non_command(self):
+        replies = self.command('balance')
+        self.assertEqual(len(replies), 0)
+
+    def test_ignore(self):
+        replies = self.command('!ignore')
+        self.assertEqual(len(replies), 0)
+
+    def test_help(self):
+        replies = self.command('!help')
+        self.assertEqual(len(replies), 1)
+        self.assertEqual(replies[0], message.HELP_ORG)
+
+class TestBalance(Test):
+    def test_balance(self):
+        self.command('!create')
+
+        replies = self.command('!balance')
+        self.assertEqual(replies[0], message.modify_balance(1000))
+
+        sess = self.Session()
+        investor = sess.query(Investor)\
+            .filter(Investor.name == 'testuser')\
+            .first()
+        investor.balance = 1234
+        sess.commit()
+
+        replies = self.command('!balance')
+        self.assertEqual(replies[0], message.modify_balance(1234))
+
+class TestBroke(Test):
+    def test_too_much_money(self):
+        self.command('!create')
+
+        replies = self.command('!broke')
+        self.assertEqual(len(replies), 1)
+        self.assertEqual(replies[0], message.modify_broke_money(1000))
+
+    # def test_active(self):
+    #     self.command('!create')
+    #     self.command('!invest 1000')
+    #
+    #     replies = self.command('!broke')
+    #     self.assertEqual(len(replies), 1)
+    #     self.assertEqual(replies[0], message.modify_broke_money(1000))
 
 if __name__ == '__main__':
     unittest.main()
--- test/mock_praw.py
+++ test/mock_praw.py
@@ -7,13 +7,26 @@ class Redditor():
     def __str__(self):
         return self.name
 
+class Submission():
+    def __init__(self, id, author=Redditor('submitter'), ups=100):
+        self.id = id
+        self.author = author
+        self.ups = ups
+
+    def __str__(self):
+        return self.id
+
 class Comment():
-    def __init__(self, author_name, body):
+    def __init__(self, author_name, body, submission=None):
         self.is_root = False
         self.author = Redditor(author_name)
         self.created_utc = time.time()
         self.body = body
         self.replies = []
+        self.submission = submission
+
+    def __str__(self):
+        return '123'
 
     def reply_wrap(self, body):
         self.replies.append(body)
