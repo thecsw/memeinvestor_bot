1741ebe641f71da5318be7c8e8dc27df66c1e8f2
Sandy <thecsw@ku.edu>
2019-01-17 20:14:24 -0600

    Made python 3 formatting, added private and tax to the table.

--- src/leaderboard.py
+++ src/leaderboard.py
@@ -18,12 +18,8 @@ from stopwatch import Stopwatch
 logging.basicConfig(level=logging.INFO)
 
 sidebar_text_org = """
-test
-123
 
-noerdy should put stuff here
-
-TODO
+/r/MemeEconomy is a place where individuals can buy, sell, share, make, and invest in memes freely. You'll also get updates on the market and be able to collaborate with other fellow meme traders.
 
 *****
 
@@ -34,6 +30,83 @@ TODO
 **Top Firms:**
 
 %TOP_FIRMS%
+
+
+###[Join the **official** Discord!](https://discord.gg/zVgD2M7)
+
+&nbsp;
+
+^(This sub is not ***only*** for templates. It is for memes in general, themed in an economic perspective. )
+
+###***[Please send us your suggestions!](https://www.reddit.com/message/compose?to=%2Fr%2FMemeEconomy)***
+
+***
+
+**Our sidebar and rules are updated frequently to stay up-to-date with the current market. Please check our sidebar often for any edits or additions you might have missed.**
+
+***
+
+**Rules:** 
+
+1a. Submissions and submission titles must be related to the meme economy. **For Example:** "I found this meme," is not OK. "I found this meme, should I buy or sell," is OK. All post titles should reference buying or selling at a minimum, and you can browse the [Investopedia Financial Dictionary](https://www.investopedia.com/dictionary/) if you need some inspiration.
+
+1b. Titles should explain why users should invest in the meme. 
+
+2. Please invest effort in submissions and post a template for your meme. Posts with low-effort, commonly used titles, and barely-relevant content are strongly frowned upon and are subject to removal.
+
+3. Please be respectful. No personal attacking. Be civil in the comments. ***This does not mean that you should report someone every time they call you a rude name or because they have a differing opinion.***
+
+4. Standard anonymity is required. Posting personal information without consent is not allowed.
+
+5. No reposting (*Within reason*). If a post is clearly shown to be a repost, it will be removed. Crossposting is allowed as long as Rule 1 is followed and the meme is presented in an economic context. If you report a repost please include a link to an earlier post with the same content to make it easier for moderators to verify and remove the repost.
+
+6. Please respect the economy. Sharing false market information (such as sharing fake screenshots) is a crime that is subject to possible punishments based on the severity of the submission.
+
+7. No spamming or advertising.
+
+8. Please keep NSFW, offensive material, and controversial topics to a minimum. Read [rule #8](https://www.reddit.com/r/MemeEconomy/about/rules/) and send a modmail if you have any questions.
+
+9. This is a forum for reputable investors only. Your account must be older than 7 days and have at least 50 comment karma to post.
+
+10. Standard [site-wide rules](https://www.reddit.com/help/contentpolicy) apply.
+
+&nbsp;
+
+*Failure to comply with any of the rules in place may result in post/comment removal or an account ban. All title-related infractions will result in a 3 day ban for the first offense, subsequent offenses will have progressively longer bans. If you need to speak with the moderators directly, please don't be afraid to [message us](https://www.reddit.com/message/compose?to=%2Fr%2FMemeEconomy).*
+
+***
+
+**Subs you might be interested in:**
+
+/r/MemeInvestor_bot 
+
+/r/grandayy
+
+/r/WholesomeMemes
+
+/r/hmmm
+
+/r/DankChristianMemes
+
+/r/2meirl4meirl
+
+/r/IncrediblesMemes
+
+/r/Bootleg_Memes
+
+/r/MemeCalendar
+
+[Meme Multireddit](https://www.reddit.com/user/loimprevisto/m/memes/)
+
+***
+
+*Please don't send modmail asking how to buy memes. We have plenty of great users willing to help you in that regard. See [**this explanatory post**](https://www.reddit.com/r/MemeEconomy/comments/5lk964/a_beginners_guide_to_the_memeeconomy_and_meme/?ref=share&ref_source=link) for a small bit of assistance.*
+
+***
+
+*We support community projects such as the [**Meme Insider**](https://memeinsider.co/) and [**Danqex**](https://www.reddit.com/r/danqex/). You might see us occasionally promote posts about them.*
+
+***
 """
 
 # TODO: add docstring
@@ -76,13 +149,14 @@ def main():
         top_users_text = "Rank|User|Net Worth\n"
         top_users_text += ":-:|:-:|:-:\n"
         for i, user in enumerate(top_users):
-            top_users_text += str(i + 1) + "|/u/" + user.name + "|" + str(user.networth) + " MC\n"
+            top_users_text += f"{i + 1}|/u/{user.name}|{user.networth} MC\n"
 
-        top_firms_text = "Rank|Firm|Total Assets|Level\n"
-        top_firms_text += ":-:|:-:|:-:|:-:\n"
+        top_firms_text = "Rank|Firm|Total Assets|Level|Tax Rate\n"
+        top_firms_text += ":-:|:-:|:-:|:-:|:-:\n"
         for i, firm in enumerate(top_firms):
-            top_firms_text += str(i + 1) + "|" + firm.name + "|" + str(firm.balance) + " MC|" + str(firm.rank + 1) + "\n"
-
+            is_private = '(**P**) ' if firm.private else ''
+            top_firms_text += f"{i + 1}|{is_private}{firm.name}|{firm.balance} MC|{firm.rank + 1}|{firm.tax}%\n"
+            
         sidebar_text = sidebar_text_org.\
             replace("%TOP_USERS%", top_users_text).\
             replace("%TOP_FIRMS%", top_firms_text)
