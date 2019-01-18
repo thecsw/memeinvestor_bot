19dcb6246bc6f4d94a1110995318abfbcf92bb08
Sandy <thecsw@ku.edu>
2019-01-18 15:49:00 -0600

    Added the help commands dictionary

---
+++ src/help_info.py
@@ -0,0 +1,29 @@
+"""
+This module will be responsible for
+providing help information on commands
+"""
+
+help_dict = {
+    'active': "",
+    'balance': "",
+    'broke': "",
+    'create': "",
+    'createfirm': "",
+    'fire': "",
+    'firm': "",
+    'grant': "",
+    'help': "",
+    'ignore': "",
+    'invest': "",
+    'invite': "",
+    'joinfirm': "",
+    'leavefirm': "",
+    'market': "",
+    'promote': "",
+    'setprivate': "",
+    'setpublic': "",
+    'template': "",
+    'top': "",
+    'upgrade': "",
+    'version': "",
+}
