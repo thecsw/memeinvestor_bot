2530575ddaec38bbf112d199af381d8ed6ed424e
Sandy <thecsw@ku.edu>
2019-03-13 15:26:39 -0500

    Fixed the json formatting error

--- api/summary/summary.go
+++ api/summary/summary.go
@@ -29,6 +29,6 @@ func Summary(w http.ResponseWriter, r *http.Request) {
 	result["investments"]["active"] = make(map[string]int)
 	result["investments"]["active"]["investments"] = investments.InvestmentsActiveReturn()
 	to_show, _ := json.Marshal(result)
-	to_add := fmt.Sprintf(`,"investors": {"top": %s},}`, investors.InvestorsTopReturn(per_page))
+	to_add := fmt.Sprintf(`,"investors": {"top": %s}}`, investors.InvestorsTopReturn(per_page))
 	fmt.Fprintf(w, "%s", string(to_show[:len(to_show)-1]) + to_add)
 }
