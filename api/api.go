a3d191f2f0991fb289e13da4b924fa51ea48ba2f
Sandy <thecsw@ku.edu>
2019-03-16 20:38:15 -0500

    Worked on the code a bit

--- api/api.go
+++ api/api.go
@@ -6,7 +6,6 @@ import (
 	"./investor"
 	"./investors"
 	"./summary"
-	"./utils"
 	"fmt"
 	"github.com/gorilla/mux"
 	"log"
@@ -14,27 +13,26 @@ import (
 )
 
 func HelloThere(w http.ResponseWriter, r *http.Request) {
-	for _, v := range utils.GetDocumentation() {
-		fmt.Fprintf(w, "%s\n", v)
-	}
+	fmt.Fprintf(w, "Welcome to u/MemeInvestor_bot official RESTful API!\n")
 	w.WriteHeader(http.StatusOK)
 }
 
 func main() {
 	log.SetPrefix("Exception caught: ")
 	r := mux.NewRouter()
+	s := server.NewServer()
 	r.HandleFunc("/", HelloThere).Methods("GET")
-	r.HandleFunc("/summary", summary.Summary).Methods("GET")
-	r.HandleFunc("/coins/invested", coins.CoinsInvested).Methods("GET")
-	r.HandleFunc("/coins/total", coins.CoinsTotal).Methods("GET")
-	r.HandleFunc("/investments", investments.Investments).Methods("GET")
-	r.HandleFunc("/investments/active", investments.InvestmentsActive).Methods("GET")
-	r.HandleFunc("/investments/total", investments.InvestmentsTotal).Methods("GET")
-	r.HandleFunc("/investments/amount", investments.InvestmentsAmount).Methods("GET")
-	r.HandleFunc("/investments/post/{post}", investments.InvestmentsPost).Methods("GET")
-	r.HandleFunc("/investor/{name}", investor.Investor).Methods("GET")
-	r.HandleFunc("/investor/{name}/investments", investor.InvestorInvestments).Methods("GET")
-	r.HandleFunc("/investor/{name}/active", investor.InvestorInvestmentsActive).Methods("GET")
-	r.HandleFunc("/investors/top", investors.InvestorsTop).Methods("GET")
+	r.HandleFunc("/summary", summary.Summary()).Methods("GET")
+	r.HandleFunc("/coins/invested", coins.CoinsInvested()).Methods("GET")
+	r.HandleFunc("/coins/total", coins.CoinsTotal()).Methods("GET")
+	r.HandleFunc("/investments", investments.Investments()).Methods("GET")
+	r.HandleFunc("/investments/active", investments.InvestmentsActive()).Methods("GET")
+	r.HandleFunc("/investments/total", investments.InvestmentsTotal()).Methods("GET")
+	r.HandleFunc("/investments/amount", investments.InvestmentsAmount()).Methods("GET")
+	r.HandleFunc("/investments/post/{post}", investments.InvestmentsPost()).Methods("GET")
+	r.HandleFunc("/investor/{name}", investor.Investor()).Methods("GET")
+	r.HandleFunc("/investor/{name}/investments", investor.InvestorInvestments()).Methods("GET")
+	r.HandleFunc("/investor/{name}/active", investor.InvestorInvestmentsActive()).Methods("GET")
+	r.HandleFunc("/investors/top", investors.InvestorsTop()).Methods("GET")
 	log.Fatal(http.ListenAndServe(":5000", r))
 }
--- api/investors/investors.go
+++ api/investors/investors.go
@@ -22,52 +22,57 @@ type investor struct {
 	NetWorth  int64  `json:"networth,omitempty"`
 }
 
-func InvestorsTop(w http.ResponseWriter, r *http.Request) {
-	log.Print(r.RequestURI)
-
-	page, per_page, err := utils.GetPagination(r.RequestURI)
-	if err != nil {
-		log.Print(err)
-		return
-	}
-
-	conn, err := sql.Open("mysql", utils.GetDB())
-	if err != nil {
-		log.Print(err)
-	}
-	defer conn.Close()
-
-	query := fmt.Sprintf("SELECT Investors.id, Investors.name,  Investors.balance, Investors.completed, Investors.broke, Investors.badges, Investors.firm, Investors.firm_role, SUM(COALESCE(Investments.amount, 0)) + Investors.balance AS net_worth FROM Investors LEFT OUTER JOIN (SELECT * FROM Investments WHERE done = 0) AS Investments ON Investments.name = Investors.name GROUP BY Investors.id ORDER BY net_worth DESC LIMIT %d OFFSET %d;", per_page, per_page*page)
-	fmt.Println(query)
-	rows, err := conn.Query(query)
-	if err != nil {
-		log.Print(err)
-		return
-	}
-	defer rows.Close()
-
-	wrapper := make([]investor, 0, per_page)
-	temp := investor{}
-	for rows.Next() {
-		err := rows.Scan(
-			&temp.Id,
-			&temp.Name,
-			&temp.Balance,
-			&temp.Completed,
-			&temp.Broke,
-			&temp.Badges,
-			&temp.Firm,
-			&temp.Firm_role,
-			&temp.NetWorth,
-		)
+func InvestorsTop() func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		page, per_page := utils.GetPagination(r.RequestURI)
+		conn, err := sql.Open("mysql", utils.GetDB())
 		if err != nil {
 			log.Print(err)
 		}
-		wrapper = append(wrapper, temp)
+		defer conn.Close()
+		query := fmt.Sprintf(`
+SELECT Investors.id, Investors.name,  Investors.balance, 
+Investors.completed, Investors.broke, Investors.badges, 
+Investors.firm, Investors.firm_role, 
+SUM(COALESCE(Investments.amount, 0)) + Investors.balance 
+AS net_worth 
+FROM Investors 
+LEFT OUTER JOIN (SELECT * FROM Investments WHERE done = 0) 
+AS Investments ON Investments.name = Investors.name 
+GROUP BY Investors.id 
+ORDER BY net_worth DESC 
+LIMIT %d OFFSET %d;`, per_page, per_page*page)
+		fmt.Println(query)
+		rows, err := conn.Query(query)
+		if err != nil {
+			log.Print(err)
+			return
+		}
+		defer rows.Close()
+
+		wrapper := make([]investor, 0, per_page)
+		temp := investor{}
+		for rows.Next() {
+			err := rows.Scan(
+				&temp.Id,
+				&temp.Name,
+				&temp.Balance,
+				&temp.Completed,
+				&temp.Broke,
+				&temp.Badges,
+				&temp.Firm,
+				&temp.Firm_role,
+				&temp.NetWorth,
+			)
+			if err != nil {
+				log.Print(err)
+			}
+			wrapper = append(wrapper, temp)
+		}
+		result, _ := json.Marshal(wrapper)
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "%s", string(result))
 	}
-	result, _ := json.Marshal(wrapper)
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "%s", string(result))
 }
 
 func InvestorsTopReturn(top int) string {
@@ -76,9 +81,18 @@ func InvestorsTopReturn(top int) string {
 		log.Print(err)
 	}
 	defer conn.Close()
-
-	query := fmt.Sprintf("SELECT Investors.id, Investors.name,  Investors.balance, Investors.completed, Investors.broke, Investors.badges, Investors.firm, Investors.firm_role, SUM(COALESCE(Investments.amount, 0)) + Investors.balance AS net_worth FROM Investors LEFT OUTER JOIN (SELECT * FROM Investments WHERE done = 0) AS Investments ON Investments.name = Investors.name GROUP BY Investors.id ORDER BY net_worth DESC LIMIT %d;", top)
-	fmt.Println(query)
+	query := fmt.Sprintf(`
+SELECT Investors.id, Investors.name,  Investors.balance, 
+Investors.completed, Investors.broke, Investors.badges, 
+Investors.firm, Investors.firm_role, 
+SUM(COALESCE(Investments.amount, 0)) + Investors.balance 
+AS net_worth 
+FROM Investors 
+LEFT OUTER JOIN (SELECT * FROM Investments WHERE done = 0) 
+AS Investments ON Investments.name = Investors.name 
+GROUP BY Investors.id 
+ORDER BY net_worth DESC 
+LIMIT %d OFFSET 0;`, top)
 	rows, err := conn.Query(query)
 	if err != nil {
 		log.Print(err)
