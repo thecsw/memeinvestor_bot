30846e531d8dd9d4fa206b74b0b762bf29155212
Sandy <thecsw@ku.edu>
2019-05-22 20:48:07 -0500

    Improved the submitter.go

--- submitter/submitter.go
+++ submitter/submitter.go
@@ -2,11 +2,31 @@ package main
 
 import (
 	"fmt"
-	"github.com/thecsw/mira"
 	"os"
 	"os/signal"
-	"time"
 	"strings"
+	"time"
+
+	"github.com/thecsw/mira"
+)
+
+const (
+	StickyComment = `
+**INVESTMENTS GO HERE - ONLY DIRECT REPLIES TO ME WILL BE PROCESSED**
+
+To prevent thread spam and other natural disasters, I only respond to direct replies. Other commands will be ignored and may be penalized. Let's keep our marketplace clean!
+
+---
+- Visit [meme.market](https://meme.market) for help, market statistics, and investor profiles.
+- Visit /r/MemeInvestor_bot for questions or suggestions about me.
+- Support the project via our [patreon](https://www.patreon.com/memeinvestor_bot)
+- New user? Lost or confused? Reply '!help' to this message, or visit the [Wiki](https://www.reddit.com/r/MemeEconomy/wiki/index) for a more in depth explanation.
+`
+
+	AttachSource = `
+----
+Psst, u/%NAME%, you can invoke '!template https://imgur.com/...' command to publicly post your template!
+`
 )
 
 func main() {
@@ -14,24 +34,27 @@ func main() {
 	r, _ := mira.Init(mira.ReadCredsFromFile("login.conf"))
 	// Get handler to listen
 	c, stop := r.StreamNewPosts("memeinvestor_test")
-	fmt.Println("source | [time] | thing_id | author | submitter | time elapsed")
+	var status error = nil
+	fmt.Println("source | [time] | thing_id | author | submitter | time elapsed | status")
 	go func() {
 		for {
 			post := <-c
 			start := time.Now()
 			to_post := StickyComment
 			to_post += strings.ReplaceAll(AttachSource, "%NAME%", post.GetAuthor())
-			comment, _ := r.Comment(post.GetId(), to_post)
-			r.Distinguish(comment.GetId(), "yes", true)
+			comment, err := r.Comment(post.GetId(), to_post)
+			status = err
+			status = r.Distinguish(comment.GetId(), "yes", true)
 			finish := time.Now()
 			// Output the worker log
-			fmt.Printf("%v [%v] %v %v %v %v\n",
+			fmt.Printf("%v [%v] %v %v %v %v \"%v\"\n",
 				"submitter",
 				start.Format(time.RFC1123),
 				post.GetId(),
 				post.GetAuthor(),
 				post.GetSubreddit(),
 				finish.Sub(start),
+				status,
 			)
 		}
 	}()
@@ -39,7 +62,7 @@ func main() {
 	signal.Notify(sigint, os.Interrupt)
 	// Block until received
 	<-sigint
-	fmt.Printf("Shutting down in %v\n", r.Stream.PostListInterval * time.Second)
+	fmt.Printf("Shutting down in %v\n", r.Stream.PostListInterval*time.Second)
 	// Stop the streaming
 	stop <- true
 	// Just to be sure, sleep for a while
