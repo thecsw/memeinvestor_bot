10ebefe0dfaab08f3df61b2428b21a598bf85589
Sandy <thecsw@ku.edu>
2019-05-22 20:47:34 -0500

    Added template command

--- bot/worker.go
+++ bot/worker.go
@@ -10,28 +10,27 @@ import (
 
 func worker(r *mira.Reddit, comment mira.CommentListingDataChildren) {
 	start := time.Now()
+	var status error = nil
 	// !balance
-	balance_r, _ := regexp.Match(`!balance`, []byte(comment.GetBody()));
+	balance_r, _ := regexp.Match(`!balance`, []byte(comment.GetBody()))
 	if balance_r {
-		balance(r, comment)
+		status = balance(r, comment)
 	}
 	// !template (.+)
-	template_r, _ := regexp.Match(`!template (.+)`, []byte(comment.GetBody()));
+	template_r, _ := regexp.Match(`!template (.+)`, []byte(comment.GetBody()))
 	if template_r {
-		sub, _ := regexp.Compile(`!template (.+)`)
-		template(r, comment, sub.FindStringSubmatch(comment.GetBody())[1])
+		status = template(r, comment)
 	}
-
 	finish := time.Now()
-	source := `mira`
 	// Output the worker log
-	fmt.Printf("%v [%v] %v %v %v \"%v\" %v\n",
-		source,
+	fmt.Printf("%v [%v] %v %v %v \"%v\" %v \"%v\"\n",
+		"submitter",
 		start.Format(time.RFC1123),
 		comment.GetId(),
 		comment.GetAuthor(),
 		comment.GetSubreddit(),
 		comment.GetBody(),
 		finish.Sub(start),
+		status,
 	)
 }
