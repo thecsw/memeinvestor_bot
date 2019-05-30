4b299f68bd13bf6a3dce52b884f821486e6f7fb5
Sandy <thecsw@ku.edu>
2019-05-29 23:14:07 -0500

    Added example code that accesses postgres database

--- bot/balance.go
+++ bot/balance.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
 	"github.com/thecsw/mira"
 )
 
 func balance(r *mira.Reddit, comment mira.CommentListingDataChildren) error {
-	r.Reply(comment.GetId(), "You are rich!")
+	bal := 0
+	connStr := "user=test password='1234' dbname=db host=postgres port=5432 sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return err
+	}
+	err = db.QueryRow(`
+select balance from investors where name = $1;
+`, comment.GetAuthor()).Scan(&bal)
+	if err != nil {
+		return err
+	}
+	r.Reply(comment.GetId(), fmt.Sprintf("Your balance is %s Mc", bal))
 	return nil
 }
