9e2eeb6aa0dd315b26449925280747697e5e5b6f
Sandy <thecsw@ku.edu>
2019-06-01 17:12:46 -0500

    Temporarily deleted the http stuff

--- docs/resources/leaderboards/seasons.js
+++
@@ -1,4 +0,0 @@
-export const seasons = [
-   {name:'beta season', id:'beta', dataUrl:'./resources/leaderboards/beta.json'},
-   {name:'season 1', id:'1', dataUrl:'https://meme.market/api/investors/top?per_page=50'},
-]
