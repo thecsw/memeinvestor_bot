704b8f2362ba460b29b80fca9b0c193c68e4648b
Sandy <thecsw@ku.edu>
2019-06-08 03:46:35 -0500

    Formatted the code.

--- models/investor.go
+++ models/investor.go
@@ -12,17 +12,17 @@ func (Investor) TableName() string {
 type Investor struct {
 	gorm.Model
 
-	Name      string         `gorm:"not null;unique"`
-	Password  string         `gorm:"not null;default:''"`
-	Source    string         `gorm:"not null;default:''"`
-	Balance   int64          `gorm:"not null;default:1000"`
-	Networth  int64          `gorm:"not null;default:1000"`
-	Firm      int            `gorm:"not null;default:0"`
-	Firm_role string         `gorm:"not null;default:''"`
-	Banned    bool           `gorm:"not null;default:false"`
-	Admin     bool           `gorm:"not null;default:false"`
-	Verified  bool           `gorm:"not null;default:false"`
-	Email     string         `gorm:"not null;default:''"`
-		Broke     []BrokeHistory 
-	Badges []Badge
+	Name      string `gorm:"not null;unique"`
+	Password  string `gorm:"not null;default:''"`
+	Source    string `gorm:"not null;default:''"`
+	Balance   int64  `gorm:"not null;default:1000"`
+	Networth  int64  `gorm:"not null;default:1000"`
+	Firm      int    `gorm:"not null;default:0"`
+	Firm_role string `gorm:"not null;default:''"`
+	Banned    bool   `gorm:"not null;default:false"`
+	Admin     bool   `gorm:"not null;default:false"`
+	Verified  bool   `gorm:"not null;default:false"`
+	Email     string `gorm:"not null;default:''"`
+	Broke     []BrokeHistory
+	Badges    []Badge
 }
